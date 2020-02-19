024cf80cdea8f4a0edb21a0a47c01f913f3895b7
bitamara <bitamar@HAL9000.localdomain>
2020-02-19 22:40:38 +0100

    Početni ekran

---
+++ Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        private List<Button> buttons = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Button gumb = new Button();
+                buttons.Add(gumb);
+                gumb.Size = this.button1.Size;
+                if (i < 3) gumb.Location = new Point(600 + x, this.button1.Location.Y);
+                else gumb.Location = new Point(buttons[1].Location.X, this.button2.Location.Y);
+                x += gumb.Size.Width + 10;
+                gumb.AutoSize = false;
+                gumb.BackColor = Color.Black;
+                gumb.ForeColor = Color.DarkRed;
+                gumb.FlatAppearance.BorderColor = Color.DarkRed;
+                gumb.FlatAppearance.BorderSize = 5;
+                gumb.FlatStyle = FlatStyle.Flat;
+                gumb.Font = new Font("Algerian", 22);
+
+                if (i == 0)
+                {
+                    gumb.Text = "EASY";
+                    gumb.Name = "easy";
+                }
+                else if (i == 1)
+                {
+                    gumb.Text = "MEDIUM";
+                    gumb.Name = "medium";
+                }
+                else if (i == 2)
+                {
+                    gumb.Text = "HARD";
+                    gumb.Name = "hard";
+                }
+
+                else
+                {
+                    gumb.Text = "SPECIAL";
+                    gumb.Name = "special";
+                }
+                
+                gumb.Click += new EventHandler(gumb_Click);
+                gumb.MouseEnter += new EventHandler(gumb_MouseEnter);
+                gumb.MouseLeave += new EventHandler(gumb_MouseLeave);
+                this.Controls.Add(gumb);
+            }
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            this.button1.BackColor = Color.DarkRed;
+            this.button1.ForeColor = Color.Black;
+            this.button1.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.button1.BackColor = Color.Black;
+            this.button1.ForeColor = Color.DarkRed;
+            this.button1.FlatAppearance.BorderColor = Color.DarkRed;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            this.button2.BackColor = Color.DarkRed;
+            this.button2.ForeColor = Color.Black;
+            this.button2.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            this.button2.BackColor = Color.Black;
+            this.button2.ForeColor = Color.DarkRed;
+            this.button2.FlatAppearance.BorderColor = Color.DarkRed;
+        }
+
+        private void gumb_MouseEnter(object sender, EventArgs e)
+        {
+            Button gumb = (sender as Button);
+            gumb.BackColor = Color.DarkRed;
+            gumb.ForeColor = Color.Black;
+            gumb.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void gumb_MouseLeave(object sender, EventArgs e)
+        {
+            Button gumb = (sender as Button);
+            gumb.BackColor = Color.Black;
+            gumb.ForeColor = Color.DarkRed;
+            gumb.FlatAppearance.BorderColor = Color.DarkRed;
+        }
+
+        private void gumb_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
