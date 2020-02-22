ed65da89a0eb9ca9b3168f5df3ab890c553c7994
bitamara <bitamar@HAL9000.localdomain>
2020-02-22 17:06:54 +0100

    Svi gridovi,timer i gumb za biljeske

--- Sudoku/Sudoku/Form1.cs
+++ Sudoku/Sudoku/Form1.cs
@@ -19,16 +19,24 @@ namespace Sudoku
         private int cellwidth;
         private int cellheight;
         private int cellnumber;
+        private Button notesb;
+        private Label notesl;
+        private Label time;
+        private Timer t;
+        private int timeElapsed;
+        private bool on;
 
         public Form1()
         {
             levels = new List<Button>();
             slevels = new List<Button>();
             grids = new List<DataGridView>();
+            t = new Timer();
 
             cellwidth = 45;
             cellheight = 45;
             cellnumber = 9;
+            timeElapsed = 0;
 
             InitializeComponent();
             DoubleBuffered = true; //za smanjenje grafičkih smetnji
@@ -52,10 +60,11 @@ namespace Sudoku
             grid.ForeColor = Color.DarkRed;
             grid.SelectionMode = DataGridViewSelectionMode.CellSelect;
             grid.EditingControlShowing += new DataGridViewEditingControlShowingEventHandler(grid_EditingControlShowing);
+            grid.CellValueChanged += new DataGridViewCellEventHandler(cell_CellValueChanged);
             return grid;
         }
 
-        private void clear_Buttons() //čisti liste gumbi
+        private void clear_All() //čisti gumbe i grid za ponovni početak igre
         {
             for (int i = 0; i < levels.Count; ++i)
             {
@@ -79,15 +88,25 @@ namespace Sudoku
 
             levels.Clear();
             slevels.Clear();
+
+            for (int i = 0; i < grids.Count; ++i)
+                this.Controls.Remove(grids[i]);
+
+            this.Controls.Remove(notesb);
+            this.Controls.Remove(notesl);
+            on = false;
+
+            if(t.Enabled == true) t.Stop();
+            this.Controls.Remove(time);
+            timeElapsed = 0;
+
+            this.Controls.Remove(grid);
+            this.label1.Visible = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            clear_Buttons();
-            this.Controls.Remove(grid);
-            for(int i = 0; i < grids.Count; ++i)
-                this.Controls.Remove(grids[i]);
-            this.label1.Visible = true;
+            clear_All();
 
             int x = 0;
             for (int i = 0; i < 4; ++i)
@@ -161,19 +180,19 @@ namespace Sudoku
 
         private void gumb_Click(object sender, EventArgs e)
         {
-            clear_Buttons();
+            clear_All();
             start_Game(sender);
         }
 
         private void gumb_SpecialClick(object sender, EventArgs e)
         {
-            clear_Buttons();
+            clear_All();
             show_SpecialLevels();
         }
 
         private void show_SpecialLevels()
         {
-            clear_Buttons();
+            clear_All();
             int x = 0;
             for (int i = 0; i < 4; ++i)
             {
@@ -223,31 +242,93 @@ namespace Sudoku
             }
         }
 
+        private void initialize_Notes()
+        {
+            notesb = new Button();
+            notesl = new Label();
+
+            notesl.Name = "notesl";
+            notesl.Text = "TOGGLE NOTES:";
+            notesl.Size = this.button1.Size;
+            notesl.AutoSize = true;
+            notesl.BackColor = Color.Black;
+            notesl.ForeColor = Color.DarkRed;
+            notesl.Font = new Font("Algerian", 22);
+            notesl.TextAlign = ContentAlignment.MiddleCenter;
+
+            notesb.Name = "notesb";
+            notesb.Text = "OFF";
+            notesb.Size = new Size((int)this.button1.Size.Width / 3, (int)this.button1.Size.Height / 3);
+            notesb.AutoSize = false;
+            notesb.BackColor = Color.Black;
+            notesb.ForeColor = Color.DarkRed;
+            notesb.FlatAppearance.BorderColor = Color.DarkRed;
+            notesb.FlatAppearance.BorderSize = 2;
+            notesb.FlatStyle = FlatStyle.Flat;
+            notesb.TextAlign = ContentAlignment.MiddleCenter;
+            notesb.Font = new Font("Algerian", 20);
+            notesb.Click += new EventHandler(notes_Click);
+            notesb.MouseEnter += new EventHandler(gumb_MouseEnter);
+            notesb.MouseLeave += new EventHandler(gumb_MouseLeave);
+
+            this.Controls.Add(notesl);
+            this.Controls.Add(notesb);
+        }
+
+        private void initialize_Time()
+        {
+            time = new Label();
+            time.Name = "time";
+            TimeSpan timespan = TimeSpan.FromMilliseconds(timeElapsed);
+            time.Text = "Time: " + timespan.ToString(@"mm\:ss");
+            time.Size = this.button1.Size;
+            time.AutoSize = true;
+            time.BackColor = Color.Black;
+            time.ForeColor = Color.DarkRed;
+            time.Font = new Font("Algerian", 22);
+            time.TextAlign = ContentAlignment.MiddleCenter;
+
+            t = new Timer();
+            t.Tick += new EventHandler(t_Tick);
+            t.Interval = 1000;
+            t.Start();
+
+            this.Controls.Add(time);
+        }
+
         private void start_Game(object sender) //pokreće igru
         {
+            initialize_Notes();
+            initialize_Time();
+
             Button gumb = (sender as Button);
 
-            if (gumb.Name == "easy" || gumb.Name == "medium" || gumb.Name == "hard") //one sve imaju tablicu 9x9
+            if (gumb.Name == "easy" || gumb.Name == "medium" || gumb.Name == "hard" || gumb.Name == "killer") //one sve imaju tablicu 9x9
             {
                 initialize_NewGrid("grid");
-                grid.Location = new Point(this.label1.Location.X, this.button1.Location.Y - 100);
+                cellwidth = 45;
+                cellheight = 45;
+                cellnumber = 9;
+                grid.Location = new Point(this.label1.Location.X, this.button1.Location.Y - 150);
                 start_NormalGame();
             }
 
             else if (gumb.Name == "16") //tablica 16x16
             {
                 initialize_NewGrid("grid");
+                cellwidth = 35;
+                cellheight = 35;
                 cellnumber = 16;
                 this.label1.Visible = false;
-                grid.Location = new Point(this.label1.Location.X - 200, this.button1.Location.Y - 350);
+                grid.Location = new Point(this.label1.Location.X - 100, this.button1.Location.Y - 300);
                 start_NormalGame();
             }
 
             else if (gumb.Name == "samurai") //samurai grid, 5 tablica 9x9
             {
-                cellnumber = 9;
                 cellwidth = 30;
                 cellheight = 30;
+                cellnumber = 9;
                 this.label1.Visible = false;
                 for (int j = 0; j < 5; ++j)
                 {
@@ -264,6 +345,9 @@ namespace Sudoku
         private void start_NormalGame()
         {
             grid.Size = new Size(cellwidth * cellnumber + 3, cellwidth * cellnumber + 3);
+            notesl.Location = new Point(this.label1.Location.X, this.grid.Location.Y + grid.Size.Height + 20);
+            notesb.Location = new Point(notesl.Location.X + notesl.Size.Width + 5, notesl.Location.Y);
+            time.Location = new Point(notesl.Location.X, notesl.Location.Y + notesl.Size.Height + 5);
 
             for (int i = 0; i < cellnumber; ++i)
             {
@@ -303,11 +387,15 @@ namespace Sudoku
         {
             Size size = new Size(cellwidth * cellnumber, cellwidth * cellnumber);
 
-            grids[0].Location = new Point(this.label1.Location.X - 300, this.button1.Location.Y - 300);
+            grids[0].Location = new Point(this.label1.Location.X - 200, this.button1.Location.Y - 350);
             grids[1].Location = new Point(grids[0].Location.X + size.Width + 3 * cellwidth, grids[0].Location.Y);
             grids[2].Location = new Point(grids[0].Location.X + 6 * cellwidth, grids[0].Location.Y + 6 * cellwidth);
             grids[3].Location = new Point(grids[0].Location.X, grids[0].Location.Y + size.Width + 3 * cellwidth);
             grids[4].Location = new Point(grids[1].Location.X, grids[3].Location.Y);
+
+            notesl.Location = new Point(grids[0].Location.X, grids[4].Location.Y + size.Height + 20);
+            notesb.Location = new Point(notesl.Location.X + notesl.Size.Width + 5, notesl.Location.Y);
+            time.Location = new Point(notesl.Location.X, notesl.Location.Y + notesl.Size.Height + 5);
 
             for (int i = 0; i < grids.Count; ++i)
             {
@@ -325,7 +413,7 @@ namespace Sudoku
                     grids[i].Rows.Add(row);
                 }
 
-                grids[i].Rows[0].Cells[0].Style.SelectionBackColor=Color.WhiteSmoke;//treba promijeniti poslije
+                grids[i].DefaultCellStyle.SelectionBackColor = Color.WhiteSmoke;
                 grids[i].Columns[2].DividerWidth = 2;
                 grids[i].Columns[5].DividerWidth = 2;
                 grids[i].Rows[2].DividerHeight = 2;
@@ -347,14 +435,52 @@ namespace Sudoku
             if (cellnumber == 9 && (!char.IsDigit(e.KeyChar) || (char.IsDigit(e.KeyChar) && char.Equals(e.KeyChar, '0'))))
             {
                 e.Handled = true;
+                return;
             }
             else if (cellnumber == 16)
             {
                 if ((!char.IsDigit(e.KeyChar) && !char.Equals(e.KeyChar, 'A') && !char.Equals(e.KeyChar, 'B') && !char.Equals(e.KeyChar, 'C') && !char.Equals(e.KeyChar, 'D') && !char.Equals(e.KeyChar, 'E') && !char.Equals(e.KeyChar, 'F') && !char.Equals(e.KeyChar, 'G')) || (char.IsDigit(e.KeyChar) && char.Equals(e.KeyChar, '0')))
                 {
                     e.Handled = true;
+                    return;
                 }
             }
+        }
+
+        private void cell_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView grid = (sender as DataGridView);
+
+            if (on)
+            {
+                //dopustiti unos više vrijednosti u jednu čeliju 
+            }
+
+            else
+            {
+                //dopustiti samo jednu vrijednost u svaku čeliju
+            }
+        }
+        //promijeni se stil unosa kada je gumb notes kliknut
+        private void notes_Click(object sender, EventArgs e)
+        {
+            if (notesb.Text == "OFF")
+            {
+                notesb.Text = "ON";
+                on = true;
+            }
+            else if (notesb.Text == "ON")
+            {
+                notesb.Text = "OFF";
+                on = false;
+            }
+        }
+        //računanje vremena
+        void t_Tick(object sender, EventArgs e)
+        {
+            timeElapsed += t.Interval;
+            TimeSpan timespan = TimeSpan.FromMilliseconds(timeElapsed);
+            time.Text = "Time: " + timespan.ToString(@"mm\:ss");
         }
     }
 }
